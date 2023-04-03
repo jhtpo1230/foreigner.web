bc50a28dd553f813c587d8d664232c59c909ce0e
KimJeongChul <jhtpo1230@skuniv.ac.kr>
2023-04-03 17:38:36 +0900

    Initial commit

---
+++ src/main/java/JCYB/foreigner/web/Controller/helloWorldController.java
@@ -0,0 +1,15 @@
+package JCYB.foreigner.web.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class helloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld(Model model) {
+        model.addAttribute("name", "Mustache");
+        return "index";
+    }
+}
