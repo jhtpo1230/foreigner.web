0b872bd1f3d48c069b921e073f928e70e9d22621
leeyunbin <kig0632@naver.com>
2023-04-10 16:29:12 +0900

    3rd commit

--- src/main/java/JCYB/foreigner/web/Controller/HomeController.java
+++ src/main/java/JCYB/foreigner/web/Controller/HomeController.java
@@ -2,9 +2,9 @@ package JCYB.foreigner.web.Controller;
 
 import JCYB.foreigner.web.DTO.culturalSpaceInfo;
 import JCYB.foreigner.web.Repository.culturalSpaceInfoRepository;
-import org.apache.tomcat.util.json.JSONParser;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -33,7 +33,7 @@ public class HomeController {
 
         try {
             String requestADDR=ADDR;
-            URL url = new URL("http://openapi.seoul.go.kr:8088/" + "6968734d486b696738304b49635a70/" +
+            URL url = new URL("http://openapi.seoul.go.kr:8088/" + "6678774f626a687437306f584b4846/" +
                     "json/culturalSpaceInfo/1/5/"+requestADDR);
             BufferedReader bf;
             bf = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
@@ -41,16 +41,20 @@ public class HomeController {
 
             JSONParser jsonParser = new JSONParser();
             JSONObject jsonObject = (JSONObject)jsonParser.parse(result);
-            JSONObject CardSubwayStatsNew = (JSONObject)jsonObject.get("CardSubwayStatsNew");
-            Long totalCount=(Long)CardSubwayStatsNew.get("list_total_count");
+            JSONObject culturalSpaceInfo = (JSONObject)jsonObject.get("culturalSpaceInfo");
+            Long totalCount=(Long)culturalSpaceInfo.get("list_total_count");
 
-            JSONObject subResult = (JSONObject)CardSubwayStatsNew.get("RESULT");
-            JSONArray infoArr = (JSONArray) CardSubwayStatsNew.get("row");
+            JSONObject subResult = (JSONObject)culturalSpaceInfo.get("RESULT");
+            JSONArray infoArr = (JSONArray) culturalSpaceInfo.get("row");
 
             for(int i=0;i<infoArr.size();i++){
                 JSONObject tmp = (JSONObject)infoArr.get(i);
-                culturalSpaceInfo infoObj = new culturalSpaceInfo(i+(long)1, (String)tmp.get("USE_DT"),(String)tmp.get("LINE_NUM"),(String)tmp.get("SUB_STA_NM"),
-                        (double)tmp.get("RIDE_PASGR_NUM"), (double)tmp.get("ALIGHT_PASGR_NUM"),(String)tmp.get("WORK_DT"));
+                culturalSpaceInfo infoObj = new culturalSpaceInfo(i+(long)1, (int)tmp.get("num"),(String)tmp.get("SUBJCODE"),(String)tmp.get("FAC_NAME"),
+                        (String)tmp.get("ADDR"), (double)tmp.get("X_COORD"), (double)tmp.get("Y_COORD"),(String)tmp.get("PHNE"), (String)tmp.get("FAX"),
+                        (String)tmp.get("HOMEPAGE"), (String)tmp.get("OPENHOUR"),(String)tmp.get("ENTR_FEE"),(String)tmp.get("CLOSEDAY"), (String)tmp.get("OPEN_DAY"),
+                        (String)tmp.get("SEAT_CNT"), (String)tmp.get("MAIN_IMG"), (String)tmp.get("ETC_DESC"), (String)tmp.get("FAC_DESC"), (String)tmp.get("ENTRFREE"),
+                        (String)tmp.get("SUBWAY"), (String)tmp.get("BUSSTOP"), (String)tmp.get("YELLOW"), (String)tmp.get("GREEN"),
+                        (String)tmp.get("BLUE"), (String)tmp.get("RED"), (String)tmp.get("AIRPORT"));
             }
 
         }catch(Exception e) {
